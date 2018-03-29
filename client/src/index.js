c72c01fe64a4ac635cb5181b16d4dc4ef9a5f8b3
Leo Qiao <qiaofeitong@hotmail.com>
2018-03-29 01:17:48 -0400

    setup initial react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<div>Hi</div>, document.getElementById("root"));
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
