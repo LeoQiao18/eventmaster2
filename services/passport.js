54dce65fffe37f780938997fd70278d5bbae50f9
Leo Qiao <qiaofeitong@hotmail.com>
2018-03-29 10:23:35 -0400

    routine commit

--- services/passport.js
+++ services/passport.js
@@ -23,26 +23,38 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // already have a record of this user
-          done(null, existingUser);
-        } else {
-          new User({
-            googleId: profile.id,
-            displayName: profile.displayName,
-            name: {
-              familyName: profile.name.familyName,
-              givenName: profile.name.givenName
-            },
-            email: profile.emails[0].value,
-            image: profile.photos[0].value
-          })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // already have a record of this user
+        // update the fields of the user
+        existingUser.displayName = profile.displayName;
+        existingUser.name = {
+          familyName: profile.name.familyName,
+          givenName: profile.name.givenName
+        };
+        existingUser.email = profile.emails[0].value;
+        existingUser.image = profile.photos[0].value;
+        // save the user document to database
+        const updatedUser = await existingUser.save();
+        // pass the user data to passport
+        done(null, updatedUser);
+      } else {
+        // it is a new google
+        // create a new user document and save it to database
+        const newUser = await new User({
+          googleId: profile.id,
+          displayName: profile.displayName,
+          name: {
+            familyName: profile.name.familyName,
+            givenName: profile.name.givenName
+          },
+          email: profile.emails[0].value,
+          image: profile.photos[0].value
+        }).save();
+        // pass the user document to passport
+        done(null, newUser);
+      }
     }
   )
 );
