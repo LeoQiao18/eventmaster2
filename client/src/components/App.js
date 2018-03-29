a6384862a5d4b2325b1af8ae6a9bceffe4d4aa94
Leo Qiao <qiaofeitong@hotmail.com>
2018-03-29 13:55:24 -0400

    add sidebar menu group

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -25,22 +25,18 @@ const App = () => {
                 }}
               />
               <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-                <Menu.Item key="1">
-                  <Icon type="user" />
-                  <span className="nav-text">nav 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Icon type="video-camera" />
-                  <span className="nav-text">nav 2</span>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <Icon type="upload" />
-                  <span className="nav-text">nav 3</span>
-                </Menu.Item>
-                <Menu.Item key="4">
-                  <Icon type="user" />
-                  <span className="nav-text">nav 4</span>
-                </Menu.Item>
+                <Menu.ItemGroup title="Admin">
+                  <Menu.Item key="1">
+                    <Icon type="user" />
+                    <span>Add Events</span>
+                  </Menu.Item>
+                </Menu.ItemGroup>
+                <Menu.ItemGroup title="Events">
+                  <Menu.Item key="2">
+                    <Icon type="user" />
+                    <span>Event 1</span>
+                  </Menu.Item>
+                </Menu.ItemGroup>
               </Menu>
             </Sider>
             <Layout>
