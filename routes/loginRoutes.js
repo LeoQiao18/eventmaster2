c3bac0ec3c203c4320b37d8cf782916cbd100a6a
Leo Qiao <qiaofeitong@hotmail.com>
2018-03-29 21:56:06 -0400

    add in redirectAuth and redirectNoAuth

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -2,7 +2,7 @@ const path = require("path");
 const redirectAuth = require("../middlewares/redirectAuth");
 
 module.exports = app => {
-  app.get("/login", (req, res) => {
+  app.get("/login", redirectAuth, (req, res) => {
     res.sendFile(path.join(__dirname + "/../public/login.html"));
   });
 };
